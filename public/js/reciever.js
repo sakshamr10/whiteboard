9f8949578a5641a3cc4625645b35355480c61859
sakshamr10 <44221811+sakshamr10@users.noreply.github.com>
2020-03-21 10:41:21 +0530

    first verison

---
+++ public/js/reciever.js
@@ -0,0 +1,34 @@
+socket.on("oncolorChange", function (color) {
+    ctx.strokeStyle = color;
+})
+
+socket.on("onstart",function(point){
+const {x,y,color,width}=point;
+ctx.strokeStyle=color;
+ctx.lineWidth=width;
+ctx.beginPath();
+ctx.moveTo(x, y);
+
+})
+socket.on("onend",function(point){
+    
+    const {x,y,color,width}=point;
+    ctx.strokeStyle=color;
+ctx.lineWidth=width;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+})
+socket.on("onredo",function(redoStack){
+    if(redoStack.length>0){
+        redraw();
+    }
+    
+})
+socket.on("onundo",function(undoStack){
+    if(undoStack.length>0){
+        redraw(); 
+    }
+})
+socket.on("ontool",function(tool){
+    handleClick(tool)
+})
